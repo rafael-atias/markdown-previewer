ceedaadc7e1e17c0f85fab5b0a285e58b4c28d6e
Rafael Atías <rafael.atias@gmail.com>
2020-03-15 09:42:41 -0300

    installed markedjs and DOMPurify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+// TODO: the textarea and the preview are should have different states
+
 import React from 'react';
+import Marked from "marked";
+import DOMPurify from "dompurify";
 import './App.css';
 
 export default class App extends React.Component {
@@ -10,6 +14,20 @@ export default class App extends React.Component {
     };
 
     this.changeHandler = this.changeHandler.bind(this);
+    this.keyHandler = this.keyHandler.bind(this);
+    //this.previewer = this.previewer.bind(this);
+  }
+
+  keyHandler(event) {
+    if (event.key.toLowerCase() === "enter") {
+      this.setState(function (state) {
+        return {
+          content: `${state.content}\n`,
+        }
+      });
+    } else {
+      this.changeHandler(event);
+    }
   }
 
   changeHandler(event) {
@@ -19,6 +37,8 @@ export default class App extends React.Component {
   }
 
   render() {
+    const html = DOMPurify.sanitize(Marked(this.state.content));
+
     return (
       <article className="App">
         <header>
@@ -26,7 +46,7 @@ export default class App extends React.Component {
         </header>
         <section>
           <textarea id="editor" className="layout" value={this.state.content} onChange={this.changeHandler}></textarea>
-          <div id="preview" className="layout">{this.state.content}</div>
+          <div id="preview" className="layout">{html}</div>
         </section>
       </article >
     );
