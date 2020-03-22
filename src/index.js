9501edb35a16956b14de35ca09a9a09e580bf574
Rafael Atías <rafael.atias@gmail.com>
2020-03-21 22:46:33 -0300

    rendered AppWrapper instead of Presentational

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import { Presentational } from "./reactReduxConnection";
+import AppWrapper from "./AppWrapper";
 import * as serviceWorker from './serviceWorker';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-
-ReactDOM.render(<Presentational />, document.getElementById('root'));
+ReactDOM.render(<AppWrapper />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
