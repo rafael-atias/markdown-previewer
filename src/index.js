76ea65f9c5a7d5e7a93458662e365336e04bf775
Rafael Atías <rafael.atias@gmail.com>
2020-03-22 08:10:42 -0300

    moved relevant files to reactComponents folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppWrapper from "./AppWrapper";
+import AppWrapper from "./reactComponents/AppWrapper";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<AppWrapper />, document.getElementById('root'));
